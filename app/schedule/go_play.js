e258c85cffc0317bb264e121bfd191d08b8c3915
wxm <wongxming@qq.com>
2017-08-30 13:05:34 +0800

    Create go_play.js

---
+++ app/schedule/go_play.js
@@ -0,0 +1,26 @@
+module.exports = {
+    
+    schedule: {
+        cron: '0 30 9 * * 2', //每周二下午7：30点触发
+        type: 'worker', 
+    },
+    
+    * task(ctx) {
+
+        var webhookUrl = 'https://oapi.dingtalk.com/robot/send?access_token=618dea7fa1504c436d46cd9686d6b81fc7e0b7c0ceb0da1d022b08e35f6b4c1e';
+        const res = yield ctx.curl(webhookUrl, {
+            method: 'POST',
+            contentType: 'json',
+            data: {
+                "msgtype": "text",
+                "text": {
+                    "content": "@所有人 ，羽毛球活动出发啦！"
+                },
+                "at": {
+                    "atMobiles": [], 
+                    "isAtAll": true
+                }
+            }
+        });
+    },
+};
